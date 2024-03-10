22986ce08b653ad082c02a36751db579086339b8
77Vishnu <116619083+77Vishnu@users.noreply.github.com>
2024-03-10 19:44:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,34 +2,10 @@ import streamlit as st
 import snowflake.connector
 from snowflake.snowpark.functions import col
 import pandas
-st.title('Zena\'s Amazing Athleisure Catalog')
-# connect to snowflake
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-cnx=st.connection("snowflake")
-#session = get_active_session()
-session=cnx.session()
-# run a snowflake query and put it all in a var called my_catalog
-my_catalog=session.table('ZENAS_ATHLEISURE_DB.PRODUCTS.CATALOG_FOR_WEBSITE').select(col('color_or_style'))
-# put the dafta into a dataframe
-df = pandas.DataFrame(my_catalog)
-# temp write the dataframe to the page so I Can see what I am working with
-# streamlit.write(df)
-# put the first column into a list
-color_list = df[0].values.tolist()
-# print(color_list)
-# Let's put a pick list here so they can pick the color
-option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
-# We'll build the image caption now, since we can
-product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
-# use the option selected to go back and get all the info from the database
-session.table('ZENAS_ATHLEISURE_DB.PRODUCTS.CATALOG_FOR_WEBSITE').select(col('direct_url', 'price', 'size_list', 'upsell_product_desc')).filter(col('color_or_style') == '" + option + "')
-df2 = my_cur.fetchone()
-streamlit.image(
-df2[0],
-width=400,
-caption= product_caption
-)
-st.write('Price: ', df2[1])
-st.write('Sizes Available: ',df2[2])
-st.write(df2[3])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
+CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
