8e1c0674faafb74eea3551d7d61330178d14829c
77Vishnu <116619083+77Vishnu@users.noreply.github.com>
2024-03-10 17:35:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list
 # We'll build the image caption now, since we can
 product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 # use the option selected to go back and get all the info from the database
-session.table(ZENAS_ATHLEISURE_DB.PRODUCTS.CATALOG_FOR_WEBSITE).select(col(direct_url, price, size_list, upsell_product_desc)).select(filter(color_or_style = '" + option + "';"))
+session.table(ZENAS_ATHLEISURE_DB.PRODUCTS.CATALOG_FOR_WEBSITE).select(col('direct_url', 'price', 'size_list', 'upsell_product_desc')).filter(col('color_or_style') = '" + option + "';")
 df2 = my_cur.fetchone()
 streamlit.image(
 df2[0],
